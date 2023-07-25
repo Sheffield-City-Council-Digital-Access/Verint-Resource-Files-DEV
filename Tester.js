fbc75467a9e843202714ef321f12dbd4696e59e4
Samuel-Coupland <122489673+Samuel-Coupland@users.noreply.github.com>
2023-07-25 11:01:01 +0100

    Update Tester.js

--- Tester.js
+++ Tester.js
@@ -139,49 +139,7 @@ function initiateReady(event, kdf, progressBar) {
     }
   );
 
- /* //Start: Google Analytics
-  //Added Google Anyltics Tag Container Tracking - included here to min rebuilding DOM
-  function loadGoogleAnalytics() {
-    var ga = document.createElement("script");
-    ga.type = "text/javascript";
-    ga.async = true;
-    ga.src = "https://www.googletagmanager.com/gtag/js?id=GTM-PBGBFQVW";
-
-    var s = document.getElementsByTagName("script")[0];
-    s.parentNode.insertBefore(ga, s);
-  }
-
-  loadGoogleAnalytics(); //Create the script
-
-  window.dataLayer = window.dataLayer || [];
-
-  function gtag() {
-    dataLayer.push(arguments);
-  }
-
-  gtag("js", new Date());
-
-  gtag("config", "UA-16453174-9");
-  //Confirmed with Google tag Assistant
-  //Finish: Google Analytics*/
-
-  function loadGoogleAnalytics(){
-var ga = document.createElement('script');
-ga.type = 'text/javascript';
-ga.async = true;
-//ga.src = 'https://www.googletagmanager.com/gtag/js?id=UA-XXXXXXXXX-X';
-    ga.src = 'https://www.googletagmanager.com/gtag/js?id=GTM-PBGBFQVW';
-var s = document.getElementsByTagName('script')[0];
-s.parentNode.insertBefore(ga, s);
-}
-loadGoogleAnalytics();
-//Create the scriptwindow.dataLayer = window.dataLayer || [];
-/*function gtag(){dataLayer.push(arguments);
-}gtag('js', new Date());
-gtag('config', 'UA-16453174-9');*/
-//Confirmed with Google tag Assistant
-
-  
+ 
 
   if (kdf.name !== "cwca_process") {
     if (kdf.form.caseid !== "") {
