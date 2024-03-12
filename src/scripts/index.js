cb0f355756c98f2aff44fff21b185cfd4a60dc67
Elliott Griffiths <elliott.griffiths@sheffield.gov.uk>
2024-03-12 20:30:27 +0000

    debugging

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -7,18 +7,18 @@ function test() {
 // import './components/formTitle.js';
 // import './components/formFooter.js';
 
-import { handleOnReadyEvent } from './eventListeners/formReady.js';
-import { handlePageChangeEvent } from './eventListeners/pageChange.js';
-import { handleFieldChangeEvent } from './eventListeners/fieldChange.js';
+// import { handleOnReadyEvent } from './eventListeners/formReady.js';
+// import { handlePageChangeEvent } from './eventListeners/pageChange.js';
+// import { handleFieldChangeEvent } from './eventListeners/fieldChange.js';
 // import { } from './eventListeners/optionSelected.js';
 // import { } from './eventListeners/mapReady.js';
 // import { } from './eventListeners/mapClicked.js';
-import { handleSuccessfulAction } from './eventListeners/actionSuccessful.js';
-import { handleFailedAction } from './eventListeners/actionFailed.js';
-import { handleObjectIdSet } from './eventListeners/objectIdSet.js';
+// import { handleSuccessfulAction } from './eventListeners/actionSuccessful.js';
+// import { handleFailedAction } from './eventListeners/actionFailed.js';
+// import { handleObjectIdSet } from './eventListeners/objectIdSet.js';
 // import { } from './eventListeners/saveSuccessful.js';
 // import { } from './eventListeners/saveFailed.js';
 // import { } from './eventListeners/formComplete.js';
 
-handleOnReadyEvent(event, kdf);
+// handleOnReadyEvent(event, kdf);
 
