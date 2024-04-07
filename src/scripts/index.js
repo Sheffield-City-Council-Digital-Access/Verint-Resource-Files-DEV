ed547c506473e16d0e70133ab60849a8c520a98f
Elliott Griffiths <elliott.griffiths@sheffield.gov.uk>
2024-04-07 19:58:52 +0100

    Sorted progress bar

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1157,18 +1157,11 @@ const updateProgressBar = currentPageIndex => {
 
     // Calculate percentage
     let percentage = 0;
-    // if (currentPageIndex > 1) {
-    // if (isLastVisiblePage) {
-    //   percentage = Math.round((currentPageIndex / visiblePages) * 100);
-    // } else {
-    percentage = Math.round(((currentPageIndex - 1) / visiblePages) * 100);
-    console.log("--percentage--", percentage);
-    console.log("--currentPageIndex--", currentPageIndex);
-    console.log("--visiblePages--", visiblePages);
-    console.log("--math--", Math.round(((currentPageIndex - 1) / visiblePages) * 100));
-    console.log(isLastVisiblePage)
-    // }
-    // }
+    if (isLastVisiblePage) {
+      percentage = Math.round(((currentPageIndex - 1) / visiblePages) * 100);
+    } else {
+      percentage = Math.round((currentPageIndex / visiblePages) * 100);
+    }
 
     // Set width, text content, colour
     if (percentage === 0) {
