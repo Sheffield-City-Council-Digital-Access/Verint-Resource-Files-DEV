ffc80bca7eab5d052fdbc00a0f344eb4b32f72d3
Elliott Griffiths <elliott.griffiths@sheffield.gov.uk>
2024-04-29 15:51:22 +0100

    debugging

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1296,7 +1296,8 @@ const updateProgressBar = currentPageIndex => {
       const pages = pageHolderDiv.querySelectorAll('.dform_page');
       console.log(pages)
       // Count visible pages
-      const visiblePages = Array.from(pages).filter(page => !page.classList.contains('dform_hidden')).length;
+      // const visiblePages = Array.from(pages).filter(page => !page.classList.contains('dform_hidden')).length;
+      const visiblePages = Array.from(pages).filter(page => !page.classList.contains('dform_hidden'));
       console.log(visiblePages)
       // Check if the last visible page is the last page in the array
       const lastVisiblePage = visiblePages[visiblePages.length - 1];
