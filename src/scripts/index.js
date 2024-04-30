57d3cb6c5e9fbd3c0c7a1413bf790dcaef5f1e59
Elliott Griffiths <elliott.griffiths@sheffield.gov.uk>
2024-04-30 14:17:23 +0100

    restoring

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1503,48 +1503,48 @@ function formatRemoveEccessWhiteSpace(value) {
 // --- FORMATING DATE AND TIME ---------------------------------------------- \\
 
 function formatDateTime(dateTime) {
-  // if (!dateTime) {
-  //   dateTime = Math.floor(Date.now() / 1000); // Use current time if no argument
-  // }
+  if (!dateTime) {
+    dateTime = Math.floor(Date.now() / 1000); // Use current time if no argument
+  }
 
-  // // Create a new Date object with options for UK locale and milliseconds
-  // const date = new Date(dateTime * 1000);
+  // Create a new Date object with options for UK locale and milliseconds
+  const date = new Date(dateTime * 1000);
 
-  // const year = date.getFullYear().toString();
-  // const month = date.getMonth().toString().padStart(2, '0');
-  // const day = date.getDate().toString().padStart(2, '0');
-  // const hours = date.getHours().toString().padStart(2, '0');
-  // const minutes = date.getMinutes().toString().padStart(2, '0');
-  // const seconds = date.getSeconds().toString().padStart(2, '0');
-  // const milliseconds = date.getMilliseconds().toString().padStart(3, '0');
+  const year = date.getFullYear().toString();
+  const month = date.getMonth().toString().padStart(2, '0');
+  const day = date.getDate().toString().padStart(2, '0');
+  const hours = date.getHours().toString().padStart(2, '0');
+  const minutes = date.getMinutes().toString().padStart(2, '0');
+  const seconds = date.getSeconds().toString().padStart(2, '0');
+  const milliseconds = date.getMilliseconds().toString().padStart(3, '0');
 
-  // return {
-  //   base: {
-  //     year: year,
-  //     month: month,
-  //     day: day,
-  //     hours: hours,
-  //     minutes: minutes,
-  //     seconds: seconds,
-  //     milliseconds: milliseconds,
-  //     weekday: {
-  //       short: date.toLocaleDateString('en-GB', { weekday: 'short' }),
-  //       long: date.toLocaleDateString('en-GB', { weekday: 'long' })
-  //     },
-  //   },
-  //   uk: {
-  //     date: date.toLocaleString('en-GB', { timeZone: 'Europe/London', year: 'numeric', month: '2-digit', day: '2-digit' }),
-  //     time: date.toLocaleString('en-GB', { timeZone: 'Europe/London', hour: '2-digit', minute: '2-digit', second: '2-digit', millisecond: '3-digit' }),
-  //     dateTime: date.toLocaleString('en-GB', { timeZone: 'Europe/London', year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit', millisecond: '3-digit' })
-  //   },
-  //   readable: {
-  //     date: date.toLocaleDateString('en-GB', { day: 'numeric', month: 'long', year: 'numeric' }),
-  //     dayDate: date.toLocaleDateString('en-GB', { weekday: 'short', day: 'numeric', month: 'long', year: 'numeric' }),
-  //     time: formatReadableTime(date)
-  //   },
-  //   iso: date.toISOString().replace(/\.\d{3}Z/, 'Z'),
-  //   utc: `${year}-${month}-${day}T${hours}:${minutes}:${seconds}.${milliseconds}Z`
-  // };
+  return {
+    base: {
+      year: year,
+      month: month,
+      day: day,
+      hours: hours,
+      minutes: minutes,
+      seconds: seconds,
+      milliseconds: milliseconds,
+      weekday: {
+        short: date.toLocaleDateString('en-GB', { weekday: 'short' }),
+        long: date.toLocaleDateString('en-GB', { weekday: 'long' })
+      },
+    },
+    uk: {
+      date: date.toLocaleString('en-GB', { timeZone: 'Europe/London', year: 'numeric', month: '2-digit', day: '2-digit' }),
+      time: date.toLocaleString('en-GB', { timeZone: 'Europe/London', hour: '2-digit', minute: '2-digit', second: '2-digit', millisecond: '3-digit' }),
+      dateTime: date.toLocaleString('en-GB', { timeZone: 'Europe/London', year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit', millisecond: '3-digit' })
+    },
+    readable: {
+      date: date.toLocaleDateString('en-GB', { day: 'numeric', month: 'long', year: 'numeric' }),
+      dayDate: date.toLocaleDateString('en-GB', { weekday: 'short', day: 'numeric', month: 'long', year: 'numeric' }),
+      time: formatReadableTime(date)
+    },
+    iso: date.toISOString().replace(/\.\d{3}Z/, 'Z'),
+    utc: `${year}-${month}-${day}T${hours}:${minutes}:${seconds}.${milliseconds}Z`
+  };
 }
 
 function formatReadableTime(date) {
