d2b326116c9e9dfc9b85e0d0dee377c26b8dcf7c
Elliott Griffiths <elliott.griffiths@sheffield.gov.uk>
2024-06-03 15:14:11 +0100

    proposing fix

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1301,7 +1301,7 @@ const updateProgressBar = currentPageIndex => {
       const visiblePages = Array.from(pages).filter(page => !page.classList.contains('dform_hidden'));
       // -1 from currentPageIndex for the active page
       // -1 from visiblePages for the confirmation page
-      let percentage = Math.round(((currentPageIndex - 1) / visiblePages.length - 1) * 100);
+      let percentage = Math.round(((currentPageIndex - 1) / (visiblePages.length - 1)) * 100);
       console.log('percentage', percentage, currentPageIndex, visiblePages.length)
       // Set width, text content, colour
       if (percentage <= 0) {
