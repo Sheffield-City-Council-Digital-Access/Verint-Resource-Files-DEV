b556ab0479a65a45ff45e33f6f3eb330f3d26f29
Elliott Griffiths <elliott.griffiths@sheffield.gov.uk>
2024-06-12 09:46:30 +0100

    adding form title to internal

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -68,6 +68,30 @@ function handleInitialisingEvent(addDateMessages) {
     targetDiv.appendChild(spinnerDiv);
   })();
 
+  // --- ADD FORM TITLE -------------------------------------------------- \\
+  (() => {
+    // Find the element with id "dform_controls"
+    const dformControls = document.getElementById("dform_controls");
+
+    // Create the new title container element
+    const titleContainer = document.createElement("div");
+    titleContainer.className = "title-container";
+
+    // Create the title element
+    const title = document.createElement("h1");
+    title.className = "form-title";
+    title.id = "form-title";
+    title.textContent = document.getElementById("dform_widget_le_title").value;
+
+    // Append the title element to the title container
+    titleContainer.appendChild(title);
+
+    // Insert the title container before the dformControls element
+    if (dformControls && dformControls.parentNode) {
+      dformControls.parentNode.insertBefore(titleContainer, dformControls);
+    }
+  })();
+
   if (KDF.kdf().access === 'citizen') {
     // --- ADD FORM HEADER ------------------------------------------------- \\
     (() => {
