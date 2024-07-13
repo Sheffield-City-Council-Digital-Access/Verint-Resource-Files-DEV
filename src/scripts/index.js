d00a182c308769be81aa44f5f4cfad93417ea4d2
Elliott Griffiths <elliott.griffiths@sheffield.gov.uk>
2024-07-13 20:28:07 +0100

    correcting month

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1765,7 +1765,7 @@ function formatDateTime(dateTime) {
   }
 
   const year = date.getFullYear().toString();
-  const month = date.getMonth().toString().padStart(2, '0');
+  const month = (date.getMonth() + 1).toString().padStart(2, '0');
   const day = date.getDate().toString().padStart(2, '0');
   const hours = date.getHours().toString().padStart(2, '0');
   const minutes = date.getMinutes().toString().padStart(2, '0');
