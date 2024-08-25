b79280688e2883b5bc7e8473108d26619f8c9b85
Elliott Griffiths <elliott.griffiths@sheffield.gov.uk>
2024-08-25 19:37:21 +0100

    adding new function

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1523,6 +1523,28 @@ const setValuesToInputFields = (aliasesAndValues) => {
   });
 };
 
+const getValuesOfInputFields = (aliasesAndValues) => {
+  const currentPageId = getCurrentPageId(); // Get the current page ID
+
+  // Iterate over each custom alias and value pair
+  aliasesAndValues.forEach(({ alias, value }) => {
+    // Get the element with the specified data-customalias attribute on the current page
+    const element = document.querySelector(`
+      #${currentPageId} input[data-customalias="${alias}"], 
+      #${currentPageId} select[data-customalias="${alias}"],
+      #${currentPageId} textarea[data-customalias="${alias}"]
+    `);
+
+    // If element is found, set its value using KDF.setVal();
+    if (element) {
+      //  Get the name of the element
+      const name = element.name;
+
+      KDF.getVal(name, value);
+    }
+  });
+};
+
 // --- SHOW / HIDE FIELD ON CURRENT PAGE ------------------------------------ \\
 
 // Function to show or hide select fields based on data-customalias attributes of selects on the current page
