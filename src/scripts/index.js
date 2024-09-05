3c0cf6d8c481ba8024be905d3bfa64e7881c1328
Elliott Griffiths <elliott.griffiths@sheffield.gov.uk>
2024-09-05 21:17:53 +0100

    crrecting

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1107,7 +1107,7 @@ function handleSelectedMapLayerEvent(event, kdf, layerName, layerAttributes) {
   }
 
   if (siteName && siteCode) {
-    console.log(!getValuesOfInputFields('fullAddress'))
+    console.log(!getValuesOfInputFields({ alias: "fullAddress" }))
     setSelectedAddress(siteName, 'show');
   }
 
