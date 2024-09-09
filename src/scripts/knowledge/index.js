ee6a0746c1e6681cec323f03a804b5a4aec92d2f
Elliott Griffiths <elliott.griffiths@sheffield.gov.uk>
2024-09-09 18:14:37 +0100

    debugging

--- src/scripts/knowledge/index.js
+++ src/scripts/knowledge/index.js
@@ -743,7 +743,10 @@ function handleOnReadyKnowledge() {
 
     function filterOptionsByLetter(letter) {
       const filteredServices = services.filter(service =>
-        service.name.toUpperCase().startsWith(letter) || service.subjects.some(subject => subject.name.toUpperCase().startsWith(letter))
+        service.subjects.some(subject =>
+          subject.name && subject.name.toUpperCase().startsWith(letter) ||
+          (subject.topics && subject.topics.some(topic => topic.name && topic.name.toUpperCase().startsWith(letter)))
+        )
       );
       createOptions(filteredServices, false);
     }
