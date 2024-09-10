f4c219ee58ca0202b16cabf5363a3a46cead6c70
Elliott Griffiths <elliott.griffiths@sheffield.gov.uk>
2024-09-10 08:43:42 +0100

    debugging maps

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2651,7 +2651,7 @@ var vmap_config = {
       "number": "18",
       "name": "ground maintenance sites",
       "title": "ground maintenance sites",
-      "layer_type": "Background",
+      "layer_type": "Display",
       "layerid": "49",
       "url": "https://utility.arcgis.com/usrsvcs/servers/25557d31a8ba43408a6ad3a0495aa290/rest/services/AGOL/Verint_PublicFaultReporting/MapServer/49",
       "popup": {}
