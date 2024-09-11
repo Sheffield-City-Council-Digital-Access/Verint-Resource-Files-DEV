fc2e2f51b384c0cacf95695edee6a8552b70c2af
Elliott Griffiths <elliott.griffiths@sheffield.gov.uk>
2024-09-11 20:53:10 +0100

    updating

--- src/scripts/knowledge/pollution-and-nuisance.js
+++ src/scripts/knowledge/pollution-and-nuisance.js
@@ -40,35 +40,43 @@ const abandonedVehicles = new ContentPaN(
   'Abandoned vehicles',
   'Report an abandoned vehicle',
   `
-    <p> We have a duty to remove abandoned vehicles.The decision as to whether
-      a vehicle is abandoned is taken by an authorised Council Officer.Abandoning
-      a vehicle is an offence under the Refuse disposal(Amenity) Act 1978 and 
-      carries a maximum fine of £2, 500 and / or 3 months imprisonment.</p >
-      <h3>Who is responsible for removing the abandoned vehicle</h3>
-      <p>Removing an abandoned vehicle is a matter for us or the police.</p>
-      <h4>Police responsibility</h4>
-      <p>
-        The police will be responsible for removing the vehicle if:
-        <ul>
-          <li>it's causing an obstruction on the highway</li>
-          <li>it's suspicious and may have been stolen</li>
-          <li>the vehicle has been burnt out</li>
-        </ul>
-      </p>
-      <p>If this fits the description of the vehicle you think has been abandoned
-      then please call the police on 101 to report it.</p>
-      <h4>Our responsibility</h4>
-      <p>If the vehicle does not fall into any of the categories above and you
-      suspect it to be abandoned, please report it to us.</p>
-      <p>
-        Signs that a vehicle has been abandoned include:
-        <ul>
-          <li>broken windows</li>
-          <li>missing number plates</li>
-          <li>flat or missing tyres</li>
-          <li>doesn't look roadworthy</li>
-          <li>looks damaged or run-down</li>
-        </ul>
+    <p>
+      We have a duty to remove abandoned vehicles.The decision as to whether
+      a vehicle is abandoned is taken by an authorised Council Officer. Abandoning
+      a vehicle is an offence under the Refuse disposal (Amenity) Act 1978 and 
+      carries a maximum fine of £2, 500 and / or 3 months imprisonment.
+    </p>
+    <h3>Who is responsible for removing the abandoned vehicle</h3>
+    <p>
+      Removing an abandoned vehicle is a matter for us or the police.
+    </p>
+    <h4>Police responsibility</h4>
+    <p>
+      The police will be responsible for removing the vehicle if:
+      <ul>
+        <li>it's causing an obstruction on the highway</li>
+        <li>it's suspicious and may have been stolen</li>
+        <li>the vehicle has been burnt out</li>
+      </ul>
+    </p>
+    <p>
+      If this fits the description of the vehicle you think has been abandoned
+      then please call the police on 101 to report it.
+    </p>
+    <h4>Our responsibility</h4>
+    <p>
+      If the vehicle does not fall into any of the categories above and you
+      suspect it to be abandoned, please report it to us.
+    </p>
+    <p>
+      Signs that a vehicle has been abandoned include:
+      <ul>
+        <li>broken windows</li>
+        <li>missing number plates</li>
+        <li>flat or missing tyres</li>
+        <li>doesn't look roadworthy</li>
+        <li>looks damaged or run-down</li>
+      </ul>
     </p>
   `,
   { buttonLabel: 'Report an abandoned vehicle', formName: 'report_abandoned_vehicle' },
