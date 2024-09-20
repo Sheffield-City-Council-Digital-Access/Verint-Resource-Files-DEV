4616242328ce555a5ca6307f6cd4d1483af4f87c
Elliott Griffiths <elliott.griffiths@sheffield.gov.uk>
2024-09-20 16:25:32 +0100

    restoring

--- src/scripts/knowledge/elections-and-referendums.js
+++ src/scripts/knowledge/elections-and-referendums.js
@@ -45,27 +45,6 @@ class ContentEaR extends CoreEaR {
 // --------- KEEP THIS AT THE TOP ------------------------------------------- \\
 // --- v - ADD SCRIPT BELOW THIS LINE - v ----------------------------------- \\
 
-// Example instantiation for topicExample1
-const informationInDifferentFormats = new ContentEaR(
-  "informationInDifferentFormats",
-  "Information in different formats",
-  "Accessing infomration in different formats",
-  "<p>example text</p>",
-  { buttonLabel: "Example 1", formName: "example_form" },
-  { typeKey: "example_transfered_to_service" },
-  { typeKey: "example_information_provided" },
-  { type: "Request", keywords: [], categories: ["Housing"] },
-  { date: "05/09/2024", name: "Elliott Griffiths" }
-);
-
-// Example instantiation for subjectExample2 (Menu with topics)
-const registeringToVote = new MenuEaR(
-  "registeringToVote",
-  "Registering to vote",
-  "add description",
-  [informationInDifferentFormats]
-);
-
 // --- ^ - ADD SCRIPT ABOVE THIS LINE - ^ ----------------------------------- \\
 // --------- KEEP THIS AT THE BOTTOM ---------------------------------------- \\
 
@@ -73,5 +52,5 @@ const electionsAndReferendums = new ServiceEaR(
   "electionsAndReferendums",
   "Elections and Referendums",
   `General elections, local elections, by-election, mayoral election, police and crime commitioner, referendums, voting…`,
-  [registeringToVote]
+  []
 );
