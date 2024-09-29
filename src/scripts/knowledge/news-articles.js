2a790038df217d973a990f98b1a4722d0b822482
Elliott Griffiths <99219139+Elliott-Griffiths@users.noreply.github.com>
2024-09-29 15:11:56 +0100

    testing

--- src/scripts/knowledge/news-articles.js
+++ src/scripts/knowledge/news-articles.js
@@ -10,46 +10,46 @@ class NewsArticle {
 }
 
 const newsArticles = [
-  new NewsArticle(
-    'Verint CRM Upgrade Coming Soon!',
-    `
-        <p><strong>We anticipate launching the new system on 18th November 2024.</strong></p>
-        <p>To ensure a smooth transition and address any questions or issues that may arise, 
-        we'll be providing <strong>two weeks of hyper support</strong> starting on 18th November 
-        2024. Our dedicated support team will be available to assist you with any concerns.</p>
-        <p><strong>Need help?</strong> Check out our helpful guides and resources:</p>
-        <ul>
-        <li><strong>FAQ:</strong> [Link to FAQ page]</li>
-        <li><strong>How-to Guides:</strong> [Link to how-to guides]</li>
-        <li><strong>Community Forums:</strong> [Link to community forums]</li>
-        </ul>
-        <p>We appreciate your patience and support as we work to enhance your experience.</p>
-        <p><strong>Stay tuned for more updates!</strong></p>
-    `,
-    'Elliott Griffiths',
-    '2024-08-15',
-    '2024-08-15',
-    '2025-01-01'
-  ),
-  new NewsArticle(
-    'New Housing system Coming Soon!',
-    `
-        <p><strong>We anticipate launching the new system on 18th November 2024.</strong></p>
-        <p>To ensure a smooth transition and address any questions or issues that may arise, 
-        we'll be providing <strong>two weeks of hyper support</strong> starting on 18th November 
-        2024. Our dedicated support team will be available to assist you with any concerns.</p>
-        <p><strong>Need help?</strong> Check out our helpful guides and resources:</p>
-        <ul>
-        <li><strong>FAQ:</strong> [Link to FAQ page]</li>
-        <li><strong>How-to Guides:</strong> [Link to how-to guides]</li>
-        <li><strong>Community Forums:</strong> [Link to community forums]</li>
-        </ul>
-        <p>We appreciate your patience and support as we work to enhance your experience.</p>
-        <p><strong>Stay tuned for more updates!</strong></p>
-    `,
-    'Elliott Griffiths',
-    '2024-08-15',
-    '2024-08-15',
-    '2025-01-01'
-  ),
+  // new NewsArticle(
+  //   'Verint CRM Upgrade Coming Soon!',
+  //   `
+  //       <p><strong>We anticipate launching the new system on 18th November 2024.</strong></p>
+  //       <p>To ensure a smooth transition and address any questions or issues that may arise, 
+  //       we'll be providing <strong>two weeks of hyper support</strong> starting on 18th November 
+  //       2024. Our dedicated support team will be available to assist you with any concerns.</p>
+  //       <p><strong>Need help?</strong> Check out our helpful guides and resources:</p>
+  //       <ul>
+  //       <li><strong>FAQ:</strong> [Link to FAQ page]</li>
+  //       <li><strong>How-to Guides:</strong> [Link to how-to guides]</li>
+  //       <li><strong>Community Forums:</strong> [Link to community forums]</li>
+  //       </ul>
+  //       <p>We appreciate your patience and support as we work to enhance your experience.</p>
+  //       <p><strong>Stay tuned for more updates!</strong></p>
+  //   `,
+  //   'Elliott Griffiths',
+  //   '2024-08-15',
+  //   '2024-08-15',
+  //   '2025-01-01'
+  // ),
+  // new NewsArticle(
+  //   'New Housing system Coming Soon!',
+  //   `
+  //       <p><strong>We anticipate launching the new system on 18th November 2024.</strong></p>
+  //       <p>To ensure a smooth transition and address any questions or issues that may arise, 
+  //       we'll be providing <strong>two weeks of hyper support</strong> starting on 18th November 
+  //       2024. Our dedicated support team will be available to assist you with any concerns.</p>
+  //       <p><strong>Need help?</strong> Check out our helpful guides and resources:</p>
+  //       <ul>
+  //       <li><strong>FAQ:</strong> [Link to FAQ page]</li>
+  //       <li><strong>How-to Guides:</strong> [Link to how-to guides]</li>
+  //       <li><strong>Community Forums:</strong> [Link to community forums]</li>
+  //       </ul>
+  //       <p>We appreciate your patience and support as we work to enhance your experience.</p>
+  //       <p><strong>Stay tuned for more updates!</strong></p>
+  //   `,
+  //   'Elliott Griffiths',
+  //   '2024-08-15',
+  //   '2024-08-15',
+  //   '2025-01-01'
+  // ),
 ];
