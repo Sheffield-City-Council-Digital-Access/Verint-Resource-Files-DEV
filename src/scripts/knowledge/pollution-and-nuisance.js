663ba6a50040083e05cb98efbd555061b8c25137
Elliott Griffiths <elliott.griffiths@sheffield.gov.uk>
2024-10-07 20:51:52 +0100

    checking

--- src/scripts/knowledge/pollution-and-nuisance.js
+++ src/scripts/knowledge/pollution-and-nuisance.js
@@ -189,63 +189,88 @@ const bedBugTreatment = new ContentPaN(
   "Bed Bug Treatment",
   "Information about Bed Bug treatment options in Sheffield",
   `
-  <h3>Commercial</h3>
-  <p>
-    For commercial enquiries about bed bug treatment, you will need to email your request to <a href="mailto:EnvironmentalServicesAdmin@sheffield.gov.uk">EnvironmentalServicesAdmin@sheffield.gov.uk</a>. We will assess your request and provide you with a quote for the treatment.
-  </p>
-  <h3>Bed Bugs Domestic</h3>
-  <p>We offer different treatment options based on your housing type:</p>
-  <h3>Council Housing</h3>
-  <p>If you live in council housing, please transfer to the back office for treatment.</p>
-  <h3>Yes: Sample Required</h3>
-  <p>
-    Before we can provide treatment for bed bugs in your domestic property, we will need a sample of the pest. 
-  </p>
-  <p>You can either bring your sample to:</p>
-  <ul>
-    <li>First Point, Howden House, 1 Union Street, Sheffield, S1 2SH</li>
-  </ul>
-  <p>Or you can post your sample to:</p>
-  <ul>
-    <li>Environmental Services, Block B, Staniforth Road Depot, Staniforth Road, Sheffield, S9 3HD</li>
-  </ul>
-  <p>Please ensure your sample is in a secure, crush-proof container, alive or dead. Attach information detailing:</p>
-  <ul>
-    <li>Your name</li>
-    <li>Your address</li>
-    <li>Your contact number and/or email</li>
-    <li>How long the insects have been present</li>
-    <li>Where in the property the insects have been found</li>
-  </ul>
-  <h3>No: Cost of Treatment</h3>
-  <p>
-    The treatment cost for Bed Bugs is £330.00. This is based on a standard size of a 3-bed semi-detached property. If your property is larger, please escalate your enquiry to receive a quote.
-  </p>
-  <p>
-    If you receive one of the following benefits, the treatment cost will be reduced to £97.00:
-  </p>
-  <ul>
-    <li>Pension Credits</li>
-    <li>Universal Credits</li>
-    <li>Housing Benefits</li>
-    <li>Income Support Allowance</li>
-    <li>Income Related Job Seekers Allowance</li>
-    <li>Income Related Employment Support Allowance</li>
-  </ul>
-  <p>
-    All prices include a call-out fee of £39. This fee applies if our officer attends the property but is unable to complete the treatment.
-  </p>
-  <p>
-    Whether you require a sample or not, please refer to the information above for sample submission details.
-  </p>
-  <h3>Bed Bugs: Health Authority, Industrial and University</h3>
-  <p>
-    For bed bug treatment enquiries related to Health Authority, Industrial, and University properties, please email your request to <a href="mailto:EnvironmentalServicesAdmin@sheffield.gov.uk">EnvironmentalServicesAdmin@sheffield.gov.uk</a>. We will assess your request and provide you with a quote for the treatment.
-  </p>
-  <h3>Bed Bugs: None of the Above</h3>
-  <p>
-    If your enquiry doesn't fall into any of the above categories, we will need to refer your report to a technician for further investigation.
-  </p>
+    <h3>Commercial</h3>
+    <p>
+      For commercial enquiries about bed bug treatment, you will need to email your
+      request to
+      <a href="mailto:EnvironmentalServicesAdmin@sheffield.gov.uk"
+        >EnvironmentalServicesAdmin@sheffield.gov.uk</a
+      >. We will assess your request and provide you with a quote for the treatment.
+    </p>
+    <h3>Council Housing</h3>
+    <p>
+      If you live in council housing, please transfer to the back office for
+      treatment.
+    </p>
+    <h3>Domestic</h3>
+    <section class="info-panel" role="region" aria-label="Information panel">
+      <h3>Sample Required</h3>
+      <p>
+        Before we can provide treatment for bed bugs in your domestic property, we
+        will need a sample of the pest.
+      </p>
+    </section>
+    <p>You can either bring your sample to:</p>
+    <address>
+      First Point,<br />Howden House,<br />1 Union Street,<br />Sheffield, S1 2SH
+    </address>
+    <p>Or you can post your sample to:</p>
+    <p>
+      Please ensure your sample is in a secure, crush-proof container, alive or
+      dead. Attach information detailing:
+    </p>
+    <address>
+      Environmental Services<br />
+      Block B, Staniforth Road Depot<br />
+      Staniforth Road<br />
+      Sheffield, S9 3HD
+    </address>
+    <ul>
+      <li>Your name</li>
+      <li>Your address</li>
+      <li>Your contact number and/or email</li>
+      <li>How long the insects have been present</li>
+      <li>Where in the property the insects have been found</li>
+    </ul>
+    <h3>Cost for Treatment</h3>
+    <p>
+      The treatment cost for Bed Bugs is £330.00. This is based on a standard size
+      of a 3-bed semi-detached property. If your property is larger, please escalate
+      your enquiry to receive a quote.
+    </p>
+    <p>
+      If you receive one of the following benefits, the treatment cost will be
+      reduced to £97.00:
+    </p>
+    <ul>
+      <li>Pension Credits</li>
+      <li>Universal Credits</li>
+      <li>Housing Benefits</li>
+      <li>Income Support Allowance</li>
+      <li>Income Related Job Seekers Allowance</li>
+      <li>Income Related Employment Support Allowance</li>
+    </ul>
+    <p>
+      All prices include a call-out fee of £39. This fee applies if our officer
+      attends the property but is unable to complete the treatment.
+    </p>
+    <p>
+      Whether you require a sample or not, please refer to the information above for
+      sample submission details.
+    </p>
+    <h3>Bed Bugs: Health Authority, Industrial and University</h3>
+    <p>
+      For bed bug treatment enquiries related to Health Authority, Industrial, and
+      University properties, please email your request to
+      <a href="mailto:EnvironmentalServicesAdmin@sheffield.gov.uk"
+        >EnvironmentalServicesAdmin@sheffield.gov.uk</a
+      >. We will assess your request and provide you with a quote for the treatment.
+    </p>
+    <h3>Bed Bugs: None of the Above</h3>
+    <p>
+      If your enquiry doesn't fall into any of the above categories, we will need to
+      refer your report to a technician for further investigation.
+    </p>
   `,
   {
     buttonLabel: "",
