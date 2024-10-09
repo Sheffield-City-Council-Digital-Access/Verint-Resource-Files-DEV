28a8701a481f05b5c3f51bb43b1522524526cd13
Elliott Griffiths <elliott.griffiths@sheffield.gov.uk>
2024-10-09 21:15:59 +0100

    updating message

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1331,7 +1331,7 @@ function handleObjectIdLoaded(event, kdf, response, type, id) {
 function handleSuccessfulAction(event, kdf, response, action, actionedby) {
   if (action === "check-for-existing-case-management-form") {
     if (response.data.existingForm === "true") {
-      KDF.showInfo("This case manamgement form already exists for this case");
+      KDF.showInfo("This case manamgement form already exists");
       const dformHolder = document.getElementById("dform_holder");
       if (dformHolder) {
         dformHolder.remove();
