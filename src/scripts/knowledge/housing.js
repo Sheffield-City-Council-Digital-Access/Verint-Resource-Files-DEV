a710fb02aaa6f116b6506b467f8a1a5c371fd05b
Elliott Griffiths <elliott.griffiths@sheffield.gov.uk>
2024-10-14 19:51:23 +0100

    adding Omliys housing updates

--- src/scripts/knowledge/housing.js
+++ src/scripts/knowledge/housing.js
@@ -54,6 +54,308 @@ class FormH extends CoreH {
 // --------- KEEP THIS AT THE TOP ------------------------------------------- \\
 // --- v - ADD SCRIPT BELOW THIS LINE - v ----------------------------------- \\
 
+const applicationChanges = new ContentH(
+  "applicationChanges",
+  "Application Changes",
+  "Info on updating housing applications, submitting ID/proof, annual renewals, restrictions, exceptions, and disqualification rules.",
+  `
+    <p>
+      Change of circumstances are now online. When you are making changes, remember
+      to click 'Submit'. ID/References may also need to be submitted before any
+      changes are accepted.
+    </p>
+    <p>
+      It is very important that you notify us of any changes in your circumstances
+      so that we can amend your application. We will inform you if this results in
+      your application moving to a different band. Depending on the change you have
+      made, you may be required to resubmit your identification/reference before
+      changes are accepted. We will notify you if this is necessary.
+    </p>
+    <p>
+      If the customer has made recent changes, the changes are likely to be awaiting
+      approval, and no further changes can be made until they are approved.
+    </p>
+
+    <details class="accordion">
+      <summary class="accordion-header">
+        <h3>Accepted ID</h3>
+        <div class="accordion-icon"></div>
+      </summary>
+      <div class="accordion-content">
+        <p>
+          You must provide two forms of identification to prove changes to your
+          application. One must be from the Proof of ID documents list, while the
+          second must provide proof of your current UK address. Photo ID is also
+          required to prove your identity. If you do not have photo ID, you must
+          provide a passport-sized photo accompanied by a birth certificate. Proof
+          of child benefit is only required where children under 16 years of age are
+          being rehoused with you.
+        </p>
+
+        <h3>Proof of ID documents</h3>
+        <ul>
+          <li>Marriage certificate</li>
+          <li>Divorce/Annulment papers</li>
+          <li>
+            Passport (this can be expired, but if the customer is an adult, an adult
+            passport must be produced)
+          </li>
+          <li>
+            Deed poll documentation for name changes (to accompany details of former
+            name, if applicable)
+          </li>
+          <li>Current bus pass with photograph</li>
+          <li>UK resident permit</li>
+          <li>ID card issued by EEA/EU countries</li>
+          <li>Home Office standard acceptance letter</li>
+          <li>Immigration status document</li>
+          <li>NASS 35</li>
+          <li>Citizenship card (with hologram and photo to prove age)</li>
+          <li>Medical card</li>
+          <li>Birth certificate/Adoption certificate</li>
+          <li>Letter from Social Worker/Probation Officer/Solicitor</li>
+          <li>Driving licence</li>
+        </ul>
+
+        <h3>Proof of Address</h3>
+        <ul>
+          <li>Recent utility bill paid in that person’s name (last quarter)</li>
+          <li>Most recent council tax statement</li>
+          <li>Driving licence</li>
+          <li>Home Office standard acceptance letter</li>
+          <li>Letter from DWP for tax credits</li>
+          <li>Bank statement from within the last 3 months</li>
+          <li>Wage slip from current employer</li>
+          <li>Life assurance/Insurance policies</li>
+          <li>Proof of all other benefits</li>
+        </ul>
+
+        <h3>Proof of Child Benefit</h3>
+        <ul>
+          <li>Child benefit award letter</li>
+          <li>
+            The last 3 consecutive months of bank statements (Only where the award
+            letter is not available)
+          </li>
+        </ul>
+      </div>
+    </details>
+
+    <details class="accordion">
+      <summary class="accordion-header">
+        <h3>Annual Renewal</h3>
+        <div class="accordion-icon"></div>
+      </summary>
+      <div class="accordion-content">
+        <p>
+          Under the Allocation Policy, customers will be required to update their
+          application on the anniversary of the date they registered.
+        </p>
+        <p>
+          If applications are not renewed within 42 days when requested, the
+          application may be cancelled.
+        </p>
+        <p>
+          Customers won’t need to submit ID for any changes they make, and this will
+          continue for the time being. This will change in the future.
+        </p>
+
+        <details class="details-accordion">
+          <summary>
+            Instructions for completing the renewal of your Housing Application
+            online
+          </summary>
+          <div class="details-accordion-content">
+            <p>
+              Log onto our website at www.sheffieldpropertyshop.org.uk and follow
+              the links from the homepage.
+            </p>
+            <p>Click ‘Log into my account’.</p>
+            <p>
+              Enter your login details: Your unique reference number (found on the
+              enclosed letter), your memorable date, and your password. Contact us
+              if you need these.
+            </p>
+            <p>
+              Once logged in, click ‘Update my details’ under the ‘Application
+              renewal required’ heading. Check your information carefully, confirm
+              that the details are correct, and submit the form. You must submit the
+              form even if there are no changes.
+            </p>
+          </div>
+        </details>
+      </div>
+    </details>
+
+    <details class="accordion">
+      <summary class="accordion-header">
+        <h3>Disqualification/Restriction</h3>
+        <div class="accordion-icon"></div>
+      </summary>
+      <div class="accordion-content">
+        <p>
+          The Allocations Policy advises of various circumstances where we will
+          reduce the preference given to applicants on the Housing Register or
+          prevent them from joining the register. These include circumstances such
+          as rent arrears or past/current anti-social behavior.
+        </p>
+        <p>
+          In the most serious debt cases, the Allocations Policy allows us to place
+          applicants into Band E or disqualify a customer from the Housing Register
+          for an initial period of 12 months.
+        </p>
+        <p>A customer can apply for a restriction to be removed if:</p>
+        <ul>
+          <li>
+            They are able to evidence that their behavior has significantly improved
+            (e.g., paying off arrears in full)
+          </li>
+          <li>They appeal the restriction (disqualification) within 21 days</li>
+          <li>They have exceptional circumstances</li>
+          <li>They need a priority</li>
+        </ul>
+        <p>
+          If an applicant is allowed back on the housing register, they will have to
+          re-register. ID and references will be required unless recently submitted.
+          Usual checks will be carried out and the appropriate band chosen.
+          Applicants will lose their original waiting time.
+        </p>
+      </div>
+    </details>
+
+    <details class="accordion">
+      <summary class="accordion-header">
+        <h3>Assistance with Change of Circumstances</h3>
+        <div class="accordion-icon"></div>
+      </summary>
+      <div class="accordion-content">
+        <p>Log on using the details below if required.</p>
+        <p>Unique Reference - 1257822</p>
+        <p>Memorable date - 25/12/2012</p>
+        <p>Password - DMrm15</p>
+        <p>
+          Advisor note: You have logged in using dummy login details. Please do not
+          amend anything on screen. Log in if required using the login details above
+          and follow the instructions:
+        </p>
+        <p>Select "My Social Housing" on the left-hand side.</p>
+        <p>
+          Click "Update your details" and navigate through the form using the "Next
+          section" buttons.
+        </p>
+        <p>Please do not submit the application.</p>
+      </div>
+    </details>
+
+    <details class="accordion">
+      <summary class="accordion-header">
+        <h3>Exceptions</h3>
+        <div class="accordion-icon"></div>
+      </summary>
+      <div class="accordion-content">
+        <p>
+          The Allocations Policy advises various circumstances in which we will
+          reduce the preference given to applicants on the Housing Register or
+          prevent them from joining the register. These include circumstances such
+          as rent arrears or past/current anti-social behavior.
+        </p>
+        <p>
+          An Exception Request allows us to make decisions on whether it is
+          appropriate to waive these restrictions in exceptional circumstances, such
+          as awarding a priority or when an applicant wishes to move to more
+          suitable accommodation using their waiting time due to a change in
+          circumstances.
+        </p>
+        <p>
+          Applicants must demonstrate exceptional circumstances and prove that they
+          have taken action to rectify issues that led to reduced preference or
+          disqualification.
+        </p>
+      </div>
+    </details>
+
+    <details class="accordion">
+      <summary class="accordion-header">
+        <h3>Name Changes</h3>
+        <div class="accordion-icon"></div>
+      </summary>
+      <div class="accordion-content">
+        <p>
+          Present three proof documents to a housing office or First Point, where ID
+          will be copied and saved. The name will be updated in our system.
+        </p>
+        <p>
+          Alternatively, send the name change in writing along with proof
+          (originals, not photocopies) to RegistrationID@sheffield.gov.uk.
+        </p>
+      </div>
+    </details>
+
+    <details class="accordion">
+      <summary class="accordion-header">
+        <h3>Split Applications</h3>
+        <div class="accordion-icon"></div>
+      </summary>
+      <div class="accordion-content">
+        <h3>Main Applicant</h3>
+        <p>
+          To remove a joint applicant, you must do this online at the Property Shop
+          website.
+        </p>
+        <h3>Joint Applicant</h3>
+        <p>
+          Speak to the Registration team to remove your National Insurance Number.
+          You will need to submit a new application and confirm the removal from the
+          previous application.
+        </p>
+      </div>
+    </details>
+  `,
+  { buttonLabel: "", formName: "" },
+  { typeKey: "" },
+  { typeKey: "" },
+  { type: "Apply", keywords: [], categories: ["Housing"] },
+  { date: "14/10/2024", name: "Omily Parkin" }
+);
+
+//apply for rehousing page to be under Rehousing menu
+const applyForRehousing = new ContentH(
+  "applyForRehousing",
+  "Rehousing",
+  "Apply for rehousing",
+  `
+    <p>
+      To apply for Social Housing you need to do so via the Property Shop website.
+      There are no paper application forms.
+    </p>
+    <p>Go online and register on www.sheffieldpropertyshop.org.uk</p>
+    <p>
+      Before you can complete an application to join the Housing Register you will
+      need to register yourself as a new household on the system.
+    </p>
+    <p>
+      Once registered you will be able to complete an application form to join the
+      Housing Register.
+    </p>
+    <p>
+      You must produce proof of ID (and Child Benefit if appropriate) and a landlord
+      reference (if you are not already a Council tenant) at the same time,
+      documents can be uploaded via the website or at your local housing office or
+      First Point before your application will be validated.
+    </p>
+  `,
+  { buttonLabel: "Example 1", formName: "example_form" },
+  { typeKey: "example_transfered_to_service" },
+  { typeKey: "example_information_provided" },
+  { type: "Request", keywords: [rehousing], categories: ["Housing"] },
+  { date: "14/10/2024", name: "Omily Parkin" }
+);
+
+const rehousing = new MenuH("rehousing", "Rehousing", "TBC", [
+  applyForRehousing,
+  applicationChanges,
+]);
+
 const blockedBinChutes = new ContentH(
   "blockedBinChutes",
   "Blocked Bin Chutes",
