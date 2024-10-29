7ace068edae703fb7112b624857088b51d4d8603
Joseph Coupland <91544989+JosephCoupland@users.noreply.github.com>
2024-10-29 16:21:52 +0000

    Update feedback-and-complaints.js
    Removed the menu object
    
    Signed-off-by: Joseph Coupland <91544989+JosephCoupland@users.noreply.github.com>

--- src/scripts/knowledge/feedback-and-complaints.js
+++ src/scripts/knowledge/feedback-and-complaints.js
@@ -445,18 +445,6 @@ Within 5 working days of receipt of the complaint.
   { date: "25/10/2024", name: "George Whitehouse" }
 );
 
-const feedbackAndComplaintsMenu = new MenuH(
-  "feedbackAndComplaintsMenu",
-  "Feedback and Complaints",
-  "Description",
-  [
-    complaints,
-    childrensSocialCareComplaints,
-    adultsocialcarecomplaints,
-    corporateComplaintsProcedure,
-  ]
-);
-
 // --- ^ - ADD SCRIPT ABOVE THIS LINE - ^ ----------------------------------- \\
 // --------- KEEP THIS AT THE BOTTOM ---------------------------------------- \\
 
@@ -464,5 +452,5 @@ const feedbackAndComplaints = new ServiceFaC(
   "feedbackAndComplaints",
   "Feedback and Complaints",
   "Description",
-    [complaints, childrensSocialCareComplaints, adultsocialcarecomplaints, corporateComplaintsProcedure, feedbackAndComplaintsMenu]
+    [complaints, childrensSocialCareComplaints, adultsocialcarecomplaints, corporateComplaintsProcedure]
 );
