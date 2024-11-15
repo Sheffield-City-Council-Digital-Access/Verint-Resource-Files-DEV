dbf941626b6a915b5796767d11341679aac49870
Elliott Griffiths <elliott.griffiths@sheffield.gov.uk>
2024-11-15 15:50:35 +0000

    checking

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1773,20 +1773,20 @@ function checkPageProgress() {
     console.log("name", el.name);
     const name = el.name;
 
-    let isEmpty = el.value.trim() === "";
-    let isValid = el.checkValidity();
+    // let isEmpty = el.value.trim() === "";
+    // let isValid = el.checkValidity();
 
-    if (
-      name.startsWith("num_") &&
-      (name.endsWith("_dd") || name.endsWith("_mm") || name.endsWith("_yy"))
-    ) {
-      console.log("date", el.name);
-      const dateElement = $(
-        `#dform_widget_${name.replace("_num_", "_dt_").slice(0, -3)}`
-      );
-      isEmpty = !dateElement.val();
-      isValid = dateElement.checkValidity();
-    }
+    // if (
+    //   name.startsWith("num_") &&
+    //   (name.endsWith("_dd") || name.endsWith("_mm") || name.endsWith("_yy"))
+    // ) {
+    //   console.log("date", el.name);
+    //   const dateElement = $(
+    //     `#dform_widget_${name.replace("_num_", "_dt_").slice(0, -3)}`
+    //   );
+    //   isEmpty = !dateElement.val();
+    //   isValid = dateElement.checkValidity();
+    // }
 
     return isEmpty || !isValid;
   });
