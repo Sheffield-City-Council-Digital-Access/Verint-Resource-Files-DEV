dcfd944e995caddb224ec3d6aa97daa99b077a8d
Elliott Griffiths <elliott.griffiths@sheffield.gov.uk>
2024-11-15 16:02:16 +0000

    testing

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1784,7 +1784,7 @@ function checkPageProgress() {
       const dateElement = $(
         `#dform_widget_${name.replace("_num_", "_dt_").slice(0, -3)}`
       );
-      isEmpty = !dateElement.val().trim();
+      // isEmpty = !dateElement.val().trim();
       // isValid = dateElement.checkValidity();
     }
 
