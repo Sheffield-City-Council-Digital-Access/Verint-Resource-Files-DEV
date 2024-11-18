6a12dba0adb8ce9297e323ce2e91ae5097097606
Elliott Griffiths <elliott.griffiths@sheffield.gov.uk>
2024-11-18 12:39:01 +0000

    correcting

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2418,8 +2418,7 @@ function updateProgressBar(currentPageIndex) {
 
 function handleSetReporter(date, address) {
   // Set date to input fields and trigger change
-  console.log(date);
-  if (!date === "Invalid Date") {
+  if (date !== "Invalid Date") {
     $("#dform_widget_num_date_of_birth_dd").val(date.getDate()).blur();
     $("#dform_widget_num_date_of_birth_mm")
       .val(date.getMonth() + 1)
