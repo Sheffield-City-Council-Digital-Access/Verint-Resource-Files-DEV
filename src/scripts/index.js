d4307f8f210c02ec9755cc2ccb5bc40fa384aaff
Elliott-SCC <122038079+Elliott-SCC@users.noreply.github.com>
2025-01-24 11:07:28 +0000

    correcting

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -26,8 +26,6 @@ let fieldsToCheckBeforeClose = [];
 // Initialize an array to store the user's form page history
 const formUserPath = [];
 
-let customerId = KDF.kdf().params.customerid;
-
 // --- HANDLE INITIALISING EVENT ------------------------------------------- \\
 
 function handleInitialisingEvent() {
@@ -1130,8 +1128,12 @@ function handleOnReadyEvent(event, kdf) {
   }
 
   $("#dform_widget_button_but_view_rent_account").on("click", function () {
-    if (customerId) {
-      createModal("hubScreenRentSummary", "hub_rent_summary", customerId);
+    if (KDF.getVal("num_reporter_obj_id")) {
+      createModal(
+        "hubScreenRentSummary",
+        "hub_rent_summary",
+        KDF.getVal("num_reporter_obj_id")
+      );
     } else {
       KDF.showWarning("A customer has not been set.");
     }
@@ -1346,7 +1348,6 @@ function handleClearMapFieldsEvent() {
 function handleObjectIdSet(event, kdf, type, id) {
   KDF.setVal("txt_reporter_obj_type", type);
   KDF.setVal("num_reporter_obj_id", id);
-  customerId = id;
 
   // Update customer set state
   customerState = "agent_true";
