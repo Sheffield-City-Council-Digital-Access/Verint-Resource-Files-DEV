996e9a3f3bacf3bd85d14135721b4a1556b7d894
Joseph Coupland <91544989+JosephCoupland@users.noreply.github.com>
2025-05-14 13:39:39 +0100

    Added class for telephone test

--- src/scripts/knowledge/births-deaths-marriages-and-citizenship.js
+++ src/scripts/knowledge/births-deaths-marriages-and-citizenship.js
@@ -835,7 +835,7 @@ const registerABirth = new ContentBDMaC(
 
   <p>Please transfer using the following contact details:</p>
   <ul>
-    <li>Mobile (Warm Transfer): <a href="#" onclick="copyToClipboard('07786 126904')">07786 126904 test2</a></li>
+    <li>Mobile (Warm Transfer): <a href="#" class="telephoneNumber" onclick="copyToClipboard('07786 126904')">07786 126904 test3</a></li>
     <li>Form: Log the case using the registrars enquiry button below.</li>
   </ul>
 
