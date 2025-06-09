8f6e108973246890e9ae0ece94ae331374edf6eb
Elliott-SCC <122038079+Elliott-SCC@users.noreply.github.com>
2025-06-09 10:10:21 +0100

    added in rule to clear and reset email address in about you.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -721,25 +721,22 @@ function handleOnReadyEvent(_, kdf) {
   // --- HANDLE MANUAL ADDRESS ENTRY --------------------------------------- \\
 
   $(`.property, .street-name, .city, .postcode`).on("change", function () {
-
     const currentPageId = getCurrentPageId();
 
     const element = document.querySelector(
       `#${currentPageId} input[data-customalias="postcode"]`
     );
 
-    if (element)
-    {
+    if (element) {
       // Remove validation error styling
       element.classList.remove("dform_fielderror");
-  
+
       // Find and hide the associated validation message
       const validationMessageElement = document.querySelector(
         `#${currentPageId} div[data-name="${element.name}"] .dform_validationMessage`
       );
-  
-      if (validationMessageElement)
-      {
+
+      if (validationMessageElement) {
         validationMessageElement.style.display = "none";
       }
     }
@@ -1624,6 +1621,11 @@ function handleObjectIdLoaded(event, kdf, response, type, id) {
   }, ${response["profile-Postcode"]}`;
 
   handleSetReporter(new Date(response["profile-DateOfBirth"]), fullAddress);
+
+  // clear email field
+  KDF.setVal("eml_address", "");
+  // repopulate meial field
+  KDF.setVal("eml_address", response["profile-Email"]);
 
   // keep at the bottom
   checkPageProgress();
