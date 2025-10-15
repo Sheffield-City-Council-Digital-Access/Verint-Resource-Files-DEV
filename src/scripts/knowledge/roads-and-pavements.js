09904fb62f44fe947a34e356e92350765ff93dce
liz-taster <elizabeth.taster@sheffield.gov.uk>
2025-10-15 11:22:46 +0100

    Update roads-and-pavements.js

--- src/scripts/knowledge/roads-and-pavements.js
+++ src/scripts/knowledge/roads-and-pavements.js
@@ -192,7 +192,7 @@ const reportMudGravel = new FormRaP(
 
 const reportPotholeCrack = new FormRaP(
   "reportPotholeCrack",
-  "Pothole or crack on the highway",
+  "Pothole or crack on the highway test",
   "Report problems such as potholes or cracks in the road or pavement",
   "report_pothole_crack",
   {
