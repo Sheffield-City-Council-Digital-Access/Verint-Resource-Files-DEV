7d3ac0734cd5f904fbd6f4495cd446225e864399
Nathan Smith <nathan.smith@sheffield.gov.uk>
2025-11-04 10:26:14 +0000

    Updated to placeholder text

--- src/scripts/knowledge/news-articles.js
+++ src/scripts/knowledge/news-articles.js
@@ -18,12 +18,18 @@ class NewsArticle {
 
 const newsArticles = [
    new NewsArticle(
-    "The NEC Housing system is ready to launch today.",
+    "Lorem Ipsum",
     `
-
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+      Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut 
+      aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in 
+      voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint 
+      occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit 
+      anim id est laborum.
     `,
-    "Motolani Akinola",
-    "2024-11-03",
+    "Nathan Smith",
+    "2024-01-01",
     "2024-11-03",
     "2025-02-03"
   ),
