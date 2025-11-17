c51e70371487f1a0234b715f86696ee0429e7f52
elliott.griffiths.86 <elliott.griffiths.86@gmail.com>
2025-11-17 12:37:58 +0000

    updating new knowledge

--- knowledge/parking.js
+++ knowledge/parking.js
@@ -63,7 +63,8 @@ const makingAPayment = new ContentP(
   `
     <p>
       It is quick, safe, and secure to make a payment online or using our automated
-      telephone payment line. To do this, you will need:
+      telephone payment line. <br />
+      To do this, you will need:
     </p>
     <ul>
       <li>Ticket reference number (can be found on Gateway)</li>
@@ -2306,36 +2307,23 @@ const utilityPermit = new ContentP(
   "Utility Permit",
   "Learn about the utility permit application process, including costs and how utility companies can apply for permits in Sheffield's city centre.",
   `
-<p>
-  <li>Issued for city centre works taking place by utility companies. E.g. Yorkshire Water, BT, British Gas etc.</li>
-  <li>25 half day cards per book</li>
-  <li>Maximum of 10 books per application</li>
-</p>
 
-<p>Utility Permits are only for major utility companies who need to complete work within the city centre.</p>
-<p>Valid for am/pm slots (not full days).</p>
+<p>Utility Permits are only for major utility companies who need to complete work within the city centre. E.g. Yorkshire Water, BT, British Gas etc.</p>
 
-<p>
-  If a company is working outside the City Centre then they would need a Trade
-  Permit.
-</p>
+<p>Utility Permits are valid for am/pm slots (not full days).</p>
+
+<p> Permits are issued in books of 25 half day cards </p>
+
+<p> A maximum of 10 books can be requested per application </p>
+
+<p> If a company is working outside the City Centre then they would need a Trade Permit. </p>
+
 
 <h4>Costs</h4>
 
 <p class="cost">£193.75 for a book of 25 half day passes.</p>
 
-  <button
-      type="button"
-      class="dform_widget email-btn dform_widget_type_button"
-      aria-label="For further information and to apply send link to apply online"
-      onclick="
-        window.location.href = \`\${window.location.protocol}//\${window.location.hostname}/form/launch/send_link_to_service?\${
-          KDF.getParams().customerid ? \`customerid=\${KDF.getParams().customerid}&\` : ''
-        }interactionid=\${KDF.getParams().interactionid}&sel_service=Parking\`
-      "
-    >
-    Send link to review further information
-  </button>
+<p> For further information or to order a Utility Permit please email <a href="mailto:customerservices@sheffield.gov.uk">customerservices@sheffield.gov.uk</a> </p>
   
   <h4>Taranto</h4>
   <p>
@@ -2368,11 +2356,13 @@ const utilityPermit = new ContentP(
       "permmit",
       "permitt",
       "pemit",
-      "premit"
+      "premit",
+      "utility",
+      "utlity"
     ],
     categories: ["Parking"],
   },
-  { date: "29/07/2025", name: "Dinah Williams" }
+  { date: "16/10/2025", name: "Liz Taster" }
 );
 
 const visitorsPermits = new ContentP(
@@ -3179,6 +3169,7 @@ const howLongDoIHaveToAppeal = new ContentP(
       "parkng",
       "permt",
       "permmit",
+      "permits",
       "permitt",
       "pemit",
       "premit",
@@ -3859,6 +3850,86 @@ const unableToResolvePcn = new ContentP(
   { date: "29/07/2025", name: "Dinah Williams" }
 );
 
+const emailPcnAppealsInfoLink = new ContentP(
+  "emailPcnAppealsInfoLink",
+  "Email link to PCN appeals",
+  "Email link to SCC webpage on how to appeal a PCN.",
+  `
+<h4>Appeal a parking, bus lane, moving traffic or CAZ fine</h4>
+
+<p>
+   If you think your fine should not have been issued, or you have been charged unfairly, you can make an appeal.
+</p>
+
+<p> 
+   I am able to email you a link to the relevant page on the SCC website with information about making appeals. 
+</p>
+
+<p>Details on the page include:</p>
+<ul>
+  <li>Making an appeal</li>
+  <li>Using the interactive tool Chatbox Max to gain advice on your case</li>
+  <li>How to make an informal appeal - against a PCN left on your windscreen</li>
+  <li>How to make a formal appeal - against a Notice to Owner or PCN received in the post</li>
+  <li>What happens if you do nothing about your PCN</li>
+  <li>Debt recovery for PCNs if they remain unpaid</li>
+  <li>What to do if your vehicle has been towed</li>
+</ul>
+
+<p>
+   Information about appealing a PCN is available at: 
+   <a href="https://www.sheffield.gov.uk/parking/appeal-parking-bus-lane-moving-traffic-or-caz-fine"
+   target="_blank">https://www.sheffield.gov.uk/parking/appeal-parking-bus-lane-moving-traffic-or-caz-fine</a>
+</p>
+
+<button
+    type="button"
+    class="dform_widget email-btn dform_widget_type_button"
+    aria-label="For further information send link"
+    onclick="
+        window.location.href = \`\${window.location.protocol}//\${window.location.hostname}/form/launch/send_link_to_service?\${
+            KDF.getParams().customerid ? \`customerid=\${KDF.getParams().customerid}&\` : ''
+        }interactionid=\${KDF.getParams().interactionid}&sel_service=PCN%20Appeal%20Information\`
+    "
+>
+    Send link to review further information
+</button>
+
+
+  `,
+  { buttonLabel: "Parking Permit", formName: "" },
+  { typeKey: "" },
+  { typeKey: "penalty_charge_notice_information_provided" },
+  {
+    type: "Information",
+    keywords: [
+      "Appeal",
+      "PCN",
+      "parking",
+      "penalty notice",
+      "penalty",
+      "charge",
+      "fine",
+      "penalty charge notice",
+      "fine",
+      "ticket",
+      "parknig",
+      "parkingg",
+      "paking",
+      "parrking",
+      "parkng",
+      "bus lane",
+      "Notice to owner",
+      "CAZ",
+      "Taxi PCN",
+      "Bailiff"
+    ],
+    categories: ["Parking"],
+  },
+  { date: "12/11/2025", name: "Liz Taster" }
+);
+
+
 const penaltyChargeNoticePcn = new MenuP(
   "penaltyChargeNoticePCN",
   "Penalty Charge Notice (PCN)",
@@ -3868,6 +3939,7 @@ const penaltyChargeNoticePcn = new MenuP(
     customerPaidTwice,
     customerTriedToPayOnline,
     customerUnableToPay,
+    emailPcnAppealsInfoLink,
     enforcementBaliffEnquiries,
     howCanIAppeal,
     howLongDoIHaveToAppeal,
