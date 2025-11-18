814bd8812c41ca917ff6f12ad243d0047f46ce4b
Joseph Coupland <91544989+JosephCoupland@users.noreply.github.com>
2025-11-18 14:20:18 +0000

    Create Business.js
    Example content

---
+++ src/scripts/knowledge/Business.js
@@ -0,0 +1,108 @@
+class CoreBusiness {
+    constructor(id, name, description) {
+      this.id = id;
+      this.name = name;
+      this.description = description;
+    }
+  }
+  
+  class ServiceBusiness extends CoreBusiness {
+    constructor(id, name, description, subjects = []) {
+      super(id, name, description);
+      this.subjects = subjects;
+    }
+  }
+  
+  class MenuBusiness extends CoreBusiness {
+    constructor(id, name, description, topics = []) {
+      super(id, name, description);
+      this.topics = topics;
+    }
+  }
+  
+  class ContentBusiness extends CoreBusiness {
+    constructor(
+      id,
+      name,
+      description,
+      content,
+      process,
+      transfer,
+      finish,
+      meta,
+      lastModified
+    ) {
+      super(id, name, description);
+      this.content = content;
+      this.process = process;
+      this.transfer = transfer;
+      this.finish = finish;
+      this.meta = meta;
+      this.lastModified = lastModified;
+    }
+  }
+  
+  class FormBusiness extends CoreBusiness {
+    constructor(id, name, description, formName, meta, lastModified) {
+      super(id, name, description);
+      this.formName = formName;
+      this.meta = meta;
+      this.lastModified = lastModified;
+    }
+  }
+  
+  // --------- KEEP THIS AT THE TOP ------------------------------------------- \\
+  // --- v - ADD SCRIPT BELOW THIS LINE - v ----------------------------------- \\
+  
+  const taxiLicensingExample = new ContentP(
+    "taxiLicensingExample",
+    "Taxi Licensing Example",
+    "Information regarding taxi licensing",
+    `
+    <p>
+      Information example
+    </p>
+    `,
+  
+    { buttonLabel: "", formName: ""},
+    { typeKey: "switchboard_transferred_to_service" },
+    { typeKey: "switchboard_information_provided"},
+    {
+      type: "Request",
+      keywords: [
+        "Switchboard",
+        "Switch board",
+        "Switch",
+        "transfer",
+        "redirect",
+        "transfer call",
+        "call"
+      ],
+      categories: ["Your City Council"],
+    },
+    { date: "18/11/2025", name: "Joseph Coupland"} 
+  );
+
+  const taxiLicensing = new MenuB(
+  "taxiLicensing",
+  "Taxi Licensing",
+  "Information regarding taxi licensing",
+  [
+    taxiLicensingExample,
+  ]
+);
+  
+  // --- ^ - ADD SCRIPT ABOVE THIS LINE - ^ ----------------------------------- \\
+  // --------- KEEP THIS AT THE BOTTOM ---------------------------------------- \\
+  
+  const business = new ServiceBusiness(
+    "business",
+    "Information regarding different buisnesses such as taxi licensing",
+    "Buisness",
+      [
+        switchboard
+      ]
+  );
+
+
+  
