3c6d020274d16350c163a92bd4770a4b839e28be
Joseph Coupland <91544989+JosephCoupland@users.noreply.github.com>
2025-11-24 10:47:43 +0000

    Apply for a new hackney carriage licence

--- src/scripts/knowledge/business.js
+++ src/scripts/knowledge/business.js
@@ -54,13 +54,50 @@ class CoreBusiness {
   // --------- KEEP THIS AT THE TOP ------------------------------------------- \\
   // --- v - ADD SCRIPT BELOW THIS LINE - v ----------------------------------- \\
   
-  const taxiLicensingExample = new ContentBusiness(
-    "taxiLicensingExample",
-    "Taxi Licensing Example",
+//#region Taxi Licensing
+
+  const applyHackneyCarriage = new ContentBusiness(
+    "applyHackneyCarriage",
+    "Apply for a new hackney carriage licence",
     "Information regarding taxi licensing",
     `
     <p>
-      Information example
+      Application process has 3 stages.
+      <ol>
+        <li>Apply</li> 
+        <li>Process</li>
+        <li>Determine</li>"
+      </ol>
+    </p>
+    <h3>Test appointments</h3>
+    <p>
+        Once the applicant make an application for a Hackney Carriage Licence, the vehicle will be issued with a date to attend the compliance test. 
+        Vehicle compliance test certificate is valid for 14 days
+    </p>
+    <h3>Hackney carriage disclosure and barring checks</h3>
+    <p>
+        If the applicant does not currently hold a taxi driver licence with SCC, then they must provide a  basic Disclosure and Barring Service (DBS) certificate.<br>
+        <br>
+        If a vehicle licence is in the name of a company or a partnership, all named directors and partners within the company will need to provide us with a Basic Disclosure from the DBS.<br>
+        <br>
+        The cost for a basic DBS check is <strong>£21.50</strong>.  
+    </p>
+     <h3>Cost</h3>
+    <p>
+        A hackney carriage vehicle licence costs <strong>£212</strong>.  
+        Must pay online when complete the hackney carriage application form.
+    </p>
+    <h3>Information need</h3>
+    <p>
+        Basic DBS disclosure or your DBS certificate number if you are signed up to the online check service.<br>
+        Original V5C registration certificate (logbook) or the keepers supplement of the V5C if you have recently bought the vehicle.<br>
+        Original certificate of insurance or the insurance cover note.<br>
+        Compliance certificate.
+    </p>
+    <h3>Collecting the licence</h3>
+    <p>
+        Service will contact the applicant by email to confirm that they have issued  hackney carriage licence and arrange a time for the applicant to collect  licence from the reception at depot.
+        Arrange a time for pick by either by, email <a href="mailto:taxilicensing@sheffield.gov.uk">taxilicensing@sheffield.gov.uk</a> or telephone <a href="#" class="telephoneNumber" onclick="copyToClipboard('0114 2734264')">0114 2734264</a>.
     </p>
     `,
   
@@ -88,10 +125,13 @@ class CoreBusiness {
   "Taxi Licensing",
   "Information regarding taxi licensing",
   [
-    taxiLicensingExample,
+    applyHackneyCarriage,
   ]
 );
   
+
+//#endregion Taxi Licensing
+
   // --- ^ - ADD SCRIPT ABOVE THIS LINE - ^ ----------------------------------- \\
   // --------- KEEP THIS AT THE BOTTOM ---------------------------------------- \\
   
