9f84f19be2dfd666bb1aa7987e360ab3352ccedb
Nathan Smith <nathan.smith@sheffield.gov.uk>
2025-12-11 10:31:53 +0000

    Added security

--- src/scripts/knowledge/housing.js
+++ src/scripts/knowledge/housing.js
@@ -2856,7 +2856,7 @@ const housingOnline = new ContentH(
       Login
     </h3>
     <p>
-      Click <a href="housing.sheffield.gov.uk/ords/houlive_selfserv/f?p=13000:101::::::" target="_blank">here</a> to access Housing Online.
+      Click <a href="https://housing.sheffield.gov.uk/ords/houlive_selfserv/f?p=13000:101::::::" target="_blank">here</a> to access Housing Online.
     </p>
     <p>
       Use the following login details to access the system:
