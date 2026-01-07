241e3163d83b4677717ab6e8cabf9319f0c98ccd
Joseph Coupland <91544989+JosephCoupland@users.noreply.github.com>
2026-01-07 15:32:39 +0000

    Create Telephone.js

---
+++ src/scripts/knowledge/Telephone.js
@@ -0,0 +1,13 @@
+//Test Ideas
+
+
+// Tested Shared Content areas. \\
+const sharedInformation = `
+  <div class="shared-info">
+    <p>Information used in multiple areas test.</p>
+  <ul>
+    <li> test 5 </li>
+    <li> test 6 </li>
+  </ul>
+  </div>
+`;
