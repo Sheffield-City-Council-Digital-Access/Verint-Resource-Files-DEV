26846fb4068092747c79d7104adb4d8186355767
Joseph Coupland <91544989+JosephCoupland@users.noreply.github.com>
2026-01-07 15:59:57 +0000

    Update telephone.js
    Signed-off-by: Joseph Coupland <91544989+JosephCoupland@users.noreply.github.com>

--- src/scripts/knowledge/telephone.js
+++ src/scripts/knowledge/telephone.js
@@ -1,13 +1,1 @@
 //Test Ideas
-
-
-// Tested Shared Content areas. \\
-const sharedInformation = `
-  <div class="shared-info">
-    <p>Information used in multiple areas test.</p>
-  <ul>
-    <li> test 5 </li>
-    <li> test 6 </li>
-  </ul>
-  </div>
-`;
