1e7c23362f3837c85221cc33bfc283febfd184a3
Joseph Coupland <91544989+JosephCoupland@users.noreply.github.com>
2026-01-27 12:22:42 +0000

    Update business.js

--- src/scripts/knowledge/business.js
+++ src/scripts/knowledge/business.js
@@ -192,7 +192,7 @@ const applyPrivateHire = new ContentBusiness(
     `
     <p>
     Application process has 3 stages.
-    <ol>
+    <ol> <!-- JcHEre -->
         <li>Apply</li> 
         <li><a href="https://www.sheffield.gov.uk/taxi-licensing/private-hire-and-hackney-carriage-vehicles/hackney-carriage-vehicle-licence/choosing-hackney-carriage" target="_blank">https://www.sheffield.gov.uk/taxi-licensing/private-hire-and-hackney-carriage-vehicles/hackney-carriage-vehicle-licence/choosing-hackney-carriage</a></li>
         <li>Process</li>
