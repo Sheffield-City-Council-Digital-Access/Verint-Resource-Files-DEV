794e5d16015d2d4fcad08018aaf92a87324acd69
elliott.griffiths.86 <elliott.griffiths.86@gmail.com>
2026-03-05 21:58:08 +0000

    adding feedback

--- scripts/account.js
+++ scripts/account.js
@@ -109,10 +109,10 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     const myProfileLink = document.getElementById("nav_username");
-    if (myProfileLink) { 
+    if (myProfileLink) {
       userName = myProfileLink.textContent;
       myProfileLink.textContent = "My profile";
-  }
+    }
   })();
 
   (() => {
@@ -174,6 +174,33 @@ document.addEventListener("DOMContentLoaded", function () {
 
       footerArea.appendChild(footerLinksUl);
       footerArea.appendChild(footerCopyright);
+    
+      // --- FEEDBACK ---
+
+      const newElement = document.createElement('aside');
+      newElement.className = 'feedback-banner';
+      newElement.setAttribute('aria-labelledby', 'feedback-heading');
+
+      newElement.innerHTML = `
+        <div class="feedback-container">
+          <svg viewBox="0 0 320 280" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false">
+            <g fill="#00757f">
+              <path d="M96.489 10.39c38.01-12.94 79.97-13.76 118.53-2.64 32.72 9.5 63.14 28.9 83.17 56.76 13.14 18.05 20.83 39.99 21.59 62.32.55 22.89-6.31 45.84-19.12 64.79-15.44 23.23-38.3 40.93-63.61 52.29-23.23 10.42-48.71 15.2-74.08 15.85-23.25.07-46.38-4.03-68.45-11.26-8.03 5.01-15.67 10.7-24.28 14.74-18.37 10.34-39.36 15.56-60.34 16.54-5.91.31-11.21-5.85-9.61-11.64 1.28-4.5 5.07-7.62 7.78-11.27 11.28-14.84 19.59-32.28 21.75-50.95-15.15-17.03-25.09-38.56-28.37-61.11-4.35-25.38 2.22-51.96 15.74-73.66 18.11-28.88 47.35-49.68 79.3-60.76m6.08 19.03c-29.44 10.22-56.16 30.32-71.38 57.83-7.02 13.07-10.82 27.81-11.18 42.64.51 21.13 8.37 41.89 21.6 58.33 2.85 3.52 5.96 7.13 7.02 11.66 1.56 5.64-.02 11.41-.78 17.05-2.51 14.11-7.86 27.67-15.46 39.81 10.39-3.17 20.56-7.08 30.13-12.24 8.79-3.87 16.15-10.28 24.85-14.27 6.7-2.87 13.97-.7 20.5 1.5 34.16 11.1 71.66 10.24 105.86-.27 27.27-8.59 52.13-25.63 68.76-49.03 11-15.56 17.19-34.49 17.31-53.55-.55-14.99-4.53-29.89-11.92-42.96-6.26-11.26-14.76-21.19-24.42-29.67-19.06-17.08-43.18-27.97-68.23-32.65-30.7-6.27-62.98-4.1-92.66 5.82Z"></path>
+              <path d="M79.319 115.05c5.43 0 11.18 2.7 13.59 7.78 3.34 5.99 1.75 14.12-3.51 18.45-5.17 4.19-12.95 4.43-18.31.46-5.13-3.66-7.33-11.04-5-16.92 1.71-5.77 7.44-9.34 13.23-9.77ZM159.349 115.04c4.04.03 8.14 1.51 11.1 4.3 3.77 4.08 5.43 10.29 3.3 15.56-1.83 6.15-8.25 9.86-14.43 9.77-3.82-.44-7.69-1.85-10.34-4.73-3.47-4.09-4.93-10.04-2.89-15.12 1.71-5.78 7.46-9.35 13.26-9.78ZM239.349 115.04c5.18.05 10.74 2.46 13.24 7.22 3.72 6.02 2.27 14.51-3.15 18.99-5.18 4.22-13 4.49-18.38.48-5.11-3.68-7.3-11.05-4.96-16.93 1.71-5.76 7.45-9.33 13.25-9.76Z"></path>
+            </g>
+          </svg>
+          <div class="feedback-content">
+            <p id="feedback-heading">
+              This page is new,
+              <a href="/site/portal/form/give_feedback_suggestion" id="feedback-link" class="btn-feedback" aria-label="Provide feedback on this page">
+                please let us know if it was helpful.
+              </a>
+            </p>
+          </div>
+        </div>
+      `;
+
+      referenceDiv.parentNode.insertBefore(newElement, referenceDiv);
     }
   })();
 
@@ -297,7 +324,7 @@ document.addEventListener("DOMContentLoaded", function () {
         const notesHeader = Array.from(
           document.querySelectorAll(".le-request-header")
         ).find((header) => header.textContent.trim() === "Notes");
-        
+
         if (notesHeader) {
           notesHeader.textContent = "Request updates";
         }
